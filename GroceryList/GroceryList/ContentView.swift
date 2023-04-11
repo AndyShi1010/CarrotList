b9be2ec80fc2be4e5af2acc851ef47416506e32b
ando <andy1010999@outlook.com>
2023-04-10 20:39:33 -0700

    project init

---
+++ GroceryList/GroceryList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GroceryList
+//
+//  Created by JPL-ST-SPRING2022 on 4/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GroceryList/GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by JPL-ST-SPRING2022 on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
